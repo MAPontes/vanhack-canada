141de2f74fb071771d4d9f2f8832e8216ed9c184
Marcelo Pontes <marcelo_pontes@msn.com>
2018-03-17 23:44:18 +0000

    Fix terra

--- infra/terraform/project_variables.mk
+++ infra/terraform/project_variables.mk
@@ -1,2 +1,1 @@
 TERRAFORM_VERSION=0.11.4
-AMI_ID=$(shell aws ec2 describe-images --filters Name=tag-key,Values=app Name=tag-value,Values=myapp --query 'Images[*].{ID:ImageId}' | grep ID| cut -d " " -f10)
